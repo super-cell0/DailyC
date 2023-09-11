094990e65a6244c6788365f15a0e131c9d49efb4
chenqingsong <2895100818@qq.com>
2023-09-11 22:11:34 +0800

    daily01

---
+++ DailyC/DailyC01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DailyC
+//
+//  Created by mac on 2023/9/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
